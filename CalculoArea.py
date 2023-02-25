c2cdd34ff895b24858397ae0a6f0ac23792634d5
Mauricio-Veloso <92833176+Mauricio-Veloso@users.noreply.github.com>
2023-02-25 16:44:11 +0000

    Código adicionado.

---
+++ CalculoArea.py
@@ -0,0 +1,7 @@
+print ('Bem vindo, aqui você pode calcular a area de um quadrado, por favor sigas os paços:')
+number_1 = int(input('Digite a base do seu quadrado  '))
+number_2 = int(input('Digite a altura do seu quadrado  '))
+
+resultado = int(number_1) * int(number_2)
+
+print ('A area do seu quadrado é: ' + str(resultado))
